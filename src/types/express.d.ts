8d1e71fe4f64034ad1b7790a2cb51516d3998bda
Chaitanya <chaitanyakaushal9@gmail.com>
2026-05-27 18:57:19 +0530

    remove unused eslint disable in express types

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,6 @@
 import type { HRUser } from '@prisma/client';
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       user?: HRUser;
